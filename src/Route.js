b21bf1ff192ceff324eb2f16cb6958633b387c82
uo258865 <uo258865@uniovi.es>
2020-03-04 16:31:40 +0100

    Clase ruta para pasar el GEOJSON a ruta

---
+++ src/Route.js
@@ -0,0 +1,17 @@
+
+
+class Route{
+  constructor(name,description,points,comments,media){
+    this.name=name;
+    this.description=description;
+    this.points=points;
+    this.comments=comments;
+    this.media=media;
+  }
+  PARSER(){
+    
+  }
+  
+}
+
+export default Route;
