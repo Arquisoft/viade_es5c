e153a9f3c08a2d1889f4ea8ae9e6327c2b9b2eb6
uo258865 <uo258865@uniovi.es>
2020-03-18 10:06:26 +0100

    Ahora hay un botón para subir el fichero al pod, no se sube directamente, primer paso para el parseo

--- src/containers/AddFile/addfile.component.js
+++ src/containers/AddFile/addfile.component.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import auth from "solid-auth-client";
 import FC from 'solid-file-client';
+/*
 import Route from 'Route';
 import RouteVisualizer from 'RouteVisualizer.component'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+*/
 
 const LoadFile = (props) => {
+    let files;
 
+    const selectFile =(event)=>{
+        files=event.target.files;
+    }
+
+    /*
     const handleSubmit = async (e) => {
         const fileReader = new FileReader();
         fileReader.fileName=e.target.files[0].name;
@@ -28,7 +36,20 @@ const LoadFile = (props) => {
         ReactDOM.render(<RouteVisualizer ruta= {ruta}></RouteVisualizer>, domContainer);
         
     }
-
+    */
+    const handlerUpload = async (e) => {
+        e.preventDefault(); //Cancelar el evento
+        console.log(files[0].name);
+        const fichero=files[0];
+        const {webId} = props;
+        const fc   = new FC( auth );
+        const nombre=fichero.name;
+        const url=webId.split("profile/card#me")[0]+"rutas/"+nombre;
+        console.log(url);
+        await fc.createFile(url, fichero, "application/geo+json", {});
+        console.log("subido");
+        
+    }
     
 
     return (
@@ -36,9 +57,9 @@ const LoadFile = (props) => {
             <h1>Subir ruta</h1>
             <label>
                 Cargar ruta:
-            <input type="file" name="files[]" id="file" onChange={handleSubmit} />
+            <input type="file" name="files[]" id="file" onChange={selectFile} />
             </label> 
-           
+            <button onClick={handlerUpload}>Upload</button>
             <div id="mapa"></div>
         </div>
        
