7b092b96cb162d96d3559ae1cf6495fabcc94d71
Aitor <aitorastur99@gmail.com>
2020-03-18 09:40:19 +0100

    Added title for friends

--- src/containers/Friends/friendComponent.js
+++ src/containers/Friends/friendComponent.js
@@ -4,13 +4,14 @@ import {
     FriendList, FriendContainer
 } from './friend.style';
 import {Friend} from "./components/Entity/friend";
-
+import {useTranslation} from "react-i18next";
+const { t } = useTranslation();
 export const FriendPage = propos => {
     const {friends} = propos;
 
     return (<FriendWrapper>
         <FriendContainer className = "card">
-            <h1></h1>
+            <h1>t('navBar.friends')</h1>
             <ul>
                 {friends.map(friend => (<Friend friend={friend}/>))}
             </ul>
