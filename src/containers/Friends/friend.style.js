2a1ed610cd691372e215bdf2fbdc59e1b70787ed
Borja <uo258643@uniovi.es>
2020-03-19 11:00:48 +0100

    Compilation changes

--- src/containers/Friends/friend.style.js
+++ src/containers/Friends/friend.style.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import {media} from '../../utils';
 
 export const FriendWrapper = styled.section`
   width: 100%;
--- src/containers/Friends/friendComponent.js
+++ src/containers/Friends/friendComponent.js
@@ -1,20 +1,22 @@
 import React from "react";
 import {
     FriendWrapper,
-    FriendList, FriendContainer
+    FriendContainer
 } from './friend.style';
 import {Friend} from "./components/Entity/friend";
 import {useTranslation} from "react-i18next";
-const { t } = useTranslation();
+
 export const FriendPage = propos => {
+    const {t} = useTranslation();
     const {friends} = propos;
 
-    return (<FriendWrapper>
-        <FriendContainer className = "card">
-            <h1>t('navBar.friends')</h1>
-            <ul>
-                {friends.map(friend => (<Friend friend={friend}/>))}
-            </ul>
-        </FriendContainer>
-    </FriendWrapper>);
+    return (
+        <FriendWrapper>
+            <FriendContainer className="card">
+                <h1>{t('navBar.friends')}</h1>
+                <ul>
+                    {friends.map(friend => (<Friend friend={friend}/>))}
+                </ul>
+            </FriendContainer>
+        </FriendWrapper>);
 };
