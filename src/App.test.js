31fe9613ebe574cd0a46db81c839b0e36ce25477
uo237133 <uo237133@uniovi.es>
2020-03-26 02:14:06 +0100

    Test basicos componentes

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {cleanup, render} from 'react-testing-library';
-import App from './App';
-
-it('App renders without crashing', () => {
-  afterAll(cleanup);
-  const { container } = render(<App />);
-  expect(container).toBeTruthy();
-});
