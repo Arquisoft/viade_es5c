e4006cc73ab85628868cf4269317f1759dc2c4b3
UO258935 <UO258935@uniovi.es>
2020-03-31 14:16:02 +0200

    formulario para recoger datos compartir rutas

--- src/containers/ShareRoutes/shareroutes.component.js
+++ src/containers/ShareRoutes/shareroutes.component.js
@@ -1,46 +1,63 @@
 import React,{useState,useCallback} from "react";
 import {ShareWrapper, Input, Label} from "./shareroutes.style";
-import { NotificationTypes } from "@inrupt/solid-react-components";
-import { notification} from "../../utils";
-import {useNotification} from '@inrupt/solid-react-components';
-import {space, schema} from 'rdf-namespaces';
-import {fetchDocument} from 'tripledoc';
-const auth = require('solid-auth-client');
-
 class CreateShareRoute extends React.Component {
-     constructor(props) {
-        super(props);
-        this.friendWebID = {value: ''};
-        this.routeWebID = {value: ''};
-
-        this.handleChange = this.handleChange.bind(this);
-     }
-
-     handleChange(event) {
-        this.friendWebID({value: event.target.value});
-        this.routeWebID({value: event.target.value});
-     }
-
-     render (){
-        return (
-         <ShareWrapper>
-            <form >
-                <p>Insert the following webID's to share the route</p>
-                <Label>
-                    Route's webID:
-                    <Input type="text" value={this.routeWebID.value} onChange={this.handleChangeRoute}/>
-                </Label>
-
-                <Label>
-                    Insert your friend's webID:
-                    <Input type="text" value={this.friendWebID.value} onChange={this.handleChangeRoute}/>
-                </Label>
-                <Input type="submit" value="Submit"/>
-            </form>
-        </ShareWrapper>
-        )
+    constructor (props) {
+        super(props)
+        this.state = {
+            routeWebID: '',
+            friendWebID: ''
+        }
     }
 
-}
+    handleChange = (e) => {
+        const { name, value } = e.target
+        this.setState({ [name]: value })
+    }
 
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const values = JSON.stringify(this.state)
+        console.log(this.state.routeWebID + " " + this.state.friendWebID);
+    }
+
+    render () {
+        const { routeWebID, friendWebID } = this.state
+
+        return (
+            <ShareWrapper>
+                <div>
+                    <h1>Insert the following webID's to share the route</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <label>Route's webID:
+                                <input
+                                    type="text"
+                                    name="routeWebID"
+                                    value={routeWebID}
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>Insert your friend's webID:
+                                <input
+                                    type="text"
+                                    name="friendWebID"
+                                    value={friendWebID}
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+                        </div>
+                        <button type="submit">Send</button>
+                    </form>
+
+                    <div>
+                        <h2>Values of the form</h2>
+                        <p>{JSON.stringify(this.state)}</p>
+                    </div>
+                </div>
+            </ShareWrapper>
+        )
+    }
+}
 export default CreateShareRoute
