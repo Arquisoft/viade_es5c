545edcf4a126438af4817bc3578081b42dfc8573
UO258935 <UO258935@uniovi.es>
2020-03-31 22:54:50 +0200

    falta mandar POD

--- src/containers/ShareRoutes/shareroutes.component.js
+++ src/containers/ShareRoutes/shareroutes.component.js
@@ -1,10 +1,11 @@
 import React,{useState,useCallback} from "react";
 import {ShareWrapper, Input, Label} from "./shareroutes.style";
-import auth from 'solid-auth-client';
 import data from '@solid/query-ldflex';
-import { NotificationTypes } from "@inrupt/solid-react-components";
+import { ShareRoutesPageContent } from './shareroutes.component';
+import auth from 'solid-auth-client';
 import FC from 'solid-file-client';
-import {useNotification} from '@inrupt/solid-react-components';
+import { namedNode } from '@rdfjs/data-model';
+import { withTranslation } from "react-i18next";
 
 type Props = {webId: String};
 
@@ -12,72 +13,69 @@ class CreateShareRoute extends React.Component {
     constructor ({webId}: Props) {
         super();
         this.webID = webId.replace("profile/card#me", "");
-        console.log(this.webID);
-        //this.createNotification = useNotification(webId);
         this.state = {
             routeWebID: '',
             friendWebID: ''
         }
-        var targetUrl = this.state.friendWebID + "inbox/"; //mirar si tenemos acceso
-        console.log(targetUrl);
+        this.fc = new FC(auth);
     }
 
-    async shareRoute(friend){
-        const { t } = this.props;
+    async shareRoute(){
         try{
-            //var targetUrl = friend.webId.split("profile/card#me")[0] + "inbox/";
+            var session = await auth.currentSession();
+            console.log("session " + session);
             var targetUrl = this.state.friendWebID + "inbox/"; //mirar si tenemos acceso
             console.log(targetUrl);
-            const {createNotification} =useNotification(this.webId);
-            const licenseUrl = "";
-            const inboxes = targetUrl //lo sacado con lo de lucia
+            await this.sendMessage(this, session,targetUrl);
 
-            let content={
-                title: "Route share",
-                    summary: "has shared you a route.",
-                actor: this.webId,
-                object: this.state.routeWebID,
-                target: this.state.friendWebID
-            };
-            //await this.sendNotification(createNotification,content,inboxes.path,NotificationTypes.OFFER,
-              //  licenseUrl);
-            console.log(targetUrl);
         }catch(error) {
             console.log(error);
             alert("Could not share the route");
         }
     }
 
-    async sendNotification(createNotification,content,to,type,license){
-        try{
-            await createNotification(content,to,type,license);
-        }catch(error){
-            alert('Error sendnotification');
-        }
-    }
-
-    async sendMessage(app, session, targetUrl){
+    async sendMessage(app, session,targetUrl){
         var message = {};
         message.date = new Date(Date.now());
+
         message.id = message.date.getTime();
-        message.sender = this.webId;
+
+        message.sender = session.webId;
+        console.log("sender was " + message.sender);
+
         message.recipient = targetUrl;
+        console.log("recipient was " + message.recipient);
 
         var baseSource = session.webId.split("profile/card#me")[0];
+        console.log("baseSourse " + baseSource);
         var source = baseSource + "public/routes/";
-        message.content = source + app.getRouteName();
+        console.log("sourse " + source);
+        message.content =this.state.routeWebID;
+        console.log("message.content " + message.content);
+
         message.title = "Shared route by " + await app.getSessionName();
+        console.log("message.title " +  message.title);
+
         message.url = message.recipient + message.id + ".ttl";
-        //await app.buildMessage(session, message);
+        console.log("message.url " + message.url);
+
+        console.log("sessionWEID " + session.webId);
+        await app.buildMessage(session, message);
     }
 
     async buildMessage(session, message){
         var mess = message.url;
+        console.log("mess " + mess);
         await data[mess].schema$text.add(message.content);
+        console.log("hice el 1");
         await data[mess].rdfs$label.add(message.title);
+        console.log("hice el 2");
         await data[mess].schema$dateSent.add(message.date.toISOString());
-        //await data[mess].rdf$type.add(namedNode('https://schema.org/Message'));
-        //await data[mess].schema$sender.add(namedNode(session.webId));
+        console.log("hice el 3");
+        await data[mess].rdf$type.add(namedNode('https://schema.org/Message'));
+        console.log("hice el 4");
+        await data[mess].schema$sender.add(namedNode(session.webId));
+        console.log("hice el 5");
     }
 
     handleChange = (e) => {
@@ -90,7 +88,33 @@ class CreateShareRoute extends React.Component {
         const values = JSON.stringify(this.state)
         console.log(this.state.routeWebID + " " + this.state.friendWebID);
         this.shareRoute();
-        console.log((this.shareRoute()));
+    }
+
+    async getSessionName(){
+        var session = await auth.currentSession();
+        var tmp = session.webId.split(".")[0];
+        return tmp.split("//")[1];
+    }
+
+    getRouteNameNoExtension(){
+        var tmp = window.location.href.split("=")[1];
+        return tmp.substring(tmp.lastIndexOf('/') + 1);
+    }
+
+    getRouteName(){
+        console.log("message.contente " + this.state.routeWebID.split("/")[6]);
+        return this.state.routeWebID.split("/")[6];
+    }
+
+    async getRoute(){
+        var name = this.getRouteName();
+        var session = await auth.currentSession();
+        var url = session.webId.split("profile/card#me")[0] + "routes/";
+        console.log(url);
+        var file = await this.fc.readFile(url + name);
+        if (file != null){
+            this.setState({route: file});
+        }
     }
 
     render () {
