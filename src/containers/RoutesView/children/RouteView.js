9d5b8f2f27fa1a1626a830f10f0f6416682c4570
Aitor <aitorastur99@gmail.com>
2020-04-01 17:17:33 +0200

    Primer intento de visualizar la ruta

--- src/containers/RoutesView/children/RouteView.js
+++ src/containers/RoutesView/children/RouteView.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react';
 import {RouteCard, RouteDetail} from "./timelineroute.style";
+import CreateMap from "../../../components/MapCreate/Map";
 
-export const RouteView = props =>{
+export const RouteView = props => {
     const {ruta} = props;
-
-    return(
-        <RouteCard className="card">
+    var isToggleOn = false;
+    const showMap = () => {
+        isToggleOn = true;
+        ReactDOM.render(
+            <CreateMap parentCallback={ruta}/>,
+            document.getElementById('root')
+        );
+    };
+    return (
+        <RouteCard className="card" onclick={showMap}>
             <RouteDetail data-testid="welcome-detail">
-                <p key = {ruta.name}>{ruta.name}</p>
+                <p key={ruta.name}>{ruta.name}</p>
             </RouteDetail>
+
         </RouteCard>
     );
 };
