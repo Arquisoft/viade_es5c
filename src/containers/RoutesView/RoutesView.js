7519914d02d15cfa356cc0fc3a1d7a0681242cf2
Borja <uo258643@uniovi.es>
2020-04-01 19:35:25 +0200

    [CORRECTION] Routes work

--- src/containers/RoutesView/RoutesView.js
+++ src/containers/RoutesView/RoutesView.js
@@ -3,18 +3,13 @@ import {RouteView} from "./children/RouteView";
 import React from 'react';
 export const RoutesView = props => {
     const {rutas} = props;
-
     return (
         <RouteWrapper>
             <RouteContainer>
                 <Header>
                     <h1 className = "text--white">Rutas</h1>
                 </Header>
-
-                <ul>
-                    {rutas.map(route => (<RouteView route={route}/>))}
-                </ul>
-
+                    {rutas.map(ruta => <RouteView ruta={ruta}/>)}
             </RouteContainer>
         </RouteWrapper>
     );
--- src/containers/RoutesView/children/RouteView.js
+++ src/containers/RoutesView/children/RouteView.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react';
 import {RouteCard, RouteDetail} from "./timelineroute.style";
-import CreateMap from "../../../components/MapCreate/Map";
 
-export const RouteView = props => {
+export const RouteView = props =>{
     const {ruta} = props;
-    var isToggleOn = false;
-    const showMap = () => {
-        isToggleOn = true;
-        ReactDOM.render(
-            <CreateMap parentCallback={ruta}/>,
-            document.getElementById('root')
-        );
-    };
-    return (
-        <RouteCard className="card" onclick={showMap}>
-            <RouteDetail data-testid="welcome-detail">
-                <p key={ruta.name}>{ruta.name}</p>
-            </RouteDetail>
 
+    return(
+        <RouteCard className="card">
+            <RouteDetail data-testid="welcome-detail">
+                <p key = {ruta.name}>{ruta.name}</p>
+            </RouteDetail>
         </RouteCard>
     );
 };
