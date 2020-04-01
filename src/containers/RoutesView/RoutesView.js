06318a75acca8084f33f951598d41abb111fba87
Aitor <aitorastur99@gmail.com>
2020-04-01 19:10:34 +0200

    [BUG] Prueba solucion de rdf

--- src/containers/RoutesView/RoutesView.js
+++ src/containers/RoutesView/RoutesView.js
@@ -3,13 +3,18 @@ import {RouteView} from "./children/RouteView";
 import React from 'react';
 export const RoutesView = props => {
     const {rutas} = props;
+
     return (
         <RouteWrapper>
             <RouteContainer>
                 <Header>
                     <h1 className = "text--white">Rutas</h1>
                 </Header>
-                    {rutas.map(ruta => <RouteView ruta={ruta}/>)}
+
+                <ul>
+                    {rutas.map(route => (<RouteView route={route}/>))}
+                </ul>
+
             </RouteContainer>
         </RouteWrapper>
     );
