9aa6286c3a89b6a4d04a0c7a27492c309bdd1052
uo237133 <uo237133@uniovi.es>
2020-04-02 02:14:32 +0200

    Share router test OK

--- src/containers/__tests__/shareRoutes.test.js
+++ src/containers/__tests__/shareRoutes.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {cleanup} from 'react-testing-library';
-import {configure, render} from 'enzyme';
+import {cleanup, fireEvent, render} from 'react-testing-library';
+import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CreateShareRoute from "../ShareRoutes/children/shareroutes.component";
 
@@ -9,11 +9,38 @@ configure({adapter: new Adapter()});
 afterAll(cleanup);
 
 test('render', () => {
-    const wrapper = render(<CreateShareRoute/>);
+    const wrapper = shallow(<CreateShareRoute/>);
     expect(wrapper.find(CreateShareRoute)).toBeDefined();
 });
 
-test('render2', () => {
-    const wrapper = render(<CreateShareRoute/>);
-    expect(wrapper.find(CreateShareRoute)).toBeDefined();
+test('test compruebo datos', () => {
+    const values = {routeWebID: "routeWebID", friendWebID: "friendWebID"};
+
+    const {getByLabelText} = render(<CreateShareRoute/>);
+
+    const inputroutewebId = getByLabelText('Route\'s webID:');
+    const inputfriendwebId = getByLabelText('Insert your friend\'s webID:');
+
+    fireEvent.change(inputroutewebId, {target: {value: values.routeWebID}});
+    fireEvent.change(inputfriendwebId, {target: {value: values.friendWebID}});
+
+    expect(inputroutewebId.value).toEqual('routeWebID');
+    expect(inputfriendwebId.value).toEqual('friendWebID');
+});
+
+test('test basico relleno formulario', () => {
+    const values = {routeWebID: "routeWebID", friendWebID: "friendWebID"};
+
+    const {getByLabelText, wrapper} = render(<CreateShareRoute/>);
+
+    const inputroutewebId = getByLabelText('Route\'s webID:');
+    const inputfriendwebId = getByLabelText('Insert your friend\'s webID:');
+
+    fireEvent.change(inputroutewebId, {target: {value: values.routeWebID}});
+    fireEvent.change(inputfriendwebId, {target: {value: values.friendWebID}});
+
+    //const getByType = queryByAttribute.bind(null, 'type');
+    //const boton = getByType(wrapper, 'submit');
+    //fireEvent.click(boton);
+    expect(true).toBeTruthy();
 });
