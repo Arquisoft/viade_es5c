d1435785066550c2b17a986773103b2237b0b6a0
Aitor <aitorastur99@gmail.com>
2020-04-22 18:30:02 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- src/containers/Friends/components/Entity/friend.js
+++ src/containers/Friends/components/Entity/friend.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 export const Friend = propos => {
     const {friend} = propos;
-
+    const {t} = useTranslation();
     return (
         <tr key={friend.webId}>
             <td>{friend.name} </td>
+            <td><a href={friend.webId}>{t('friends.profile')} {friend.name}</a> </td>
         </tr>
     );
 };
