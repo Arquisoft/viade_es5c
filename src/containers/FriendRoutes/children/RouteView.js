44178168ff5caaf60b5ea8de9f052cf6c64c6cd9
UO258935 <UO258935@uniovi.es>
2020-04-27 22:02:44 +0200

    logica mostrar imagenes rutas compartidas

--- src/containers/FriendRoutes/children/RouteView.js
+++ src/containers/FriendRoutes/children/RouteView.js
@@ -13,37 +13,53 @@ export const RouteView = props => {
     //const {shareRoute} = props;
     var ruta = data.ruta;
     let comentario = "";
+    var i =0;
 
     function verMultimedia() {
-     
-       const loader = new MediaLoader();
+        const loader = new MediaLoader();
         const img = document.querySelector('#img');
-        
-       if (ruta.media.length===0){
+        const buttonR = document.querySelector('#buttonR');
+        const buttonL = document.querySelector('#buttonL');
+
+
+        if (ruta.media.length === 0) {
             ReactDOM.render(<p>{i18n.t('routeView.availability')}</p>, img);
-       }
+        }else{
 
-       for(var i=0;i<ruta.media.length;i++){
-           if(ruta.media[i].type==="video"){
-            loader.loadMedia(ruta.media[i].contentUrl,function (file) {
-                var imageUrl = URL.createObjectURL(file);
-                const img = document.querySelector('#img');
-                ReactDOM.render(<video controls className="d-block route-img" src={imageUrl} width="500" height="500"/>, img);
-            });
-           }else if(ruta.media[i].type==="image"){
-            loader.loadMedia(ruta.media[i].contentUrl,function (file) {
-                var imageUrl = URL.createObjectURL(file);
-                const img = document.querySelector('#img');
-                ReactDOM.render(<img src={imageUrl} alt={"foto" + ruta.fileName} width="500" height="500"/>, img);
-            });
-           }
-           
-       }    
+            if(i<0){
+                i=0;
+            }
+            if(i>ruta.media.length-1){
+                i=ruta.media.length-1;
+            }
+            if (ruta.media[i].type === "video") {
+                loader.loadMedia(ruta.media[i].contentUrl, function (file) {
+                    var imageUrl = URL.createObjectURL(file);
+                    // const img = document.querySelector('#img');
+                    ReactDOM.render(<video controls className="d-block route-img" src={imageUrl} width="500"
+                                           height="500"/>, img);
+                });
+            } else if (ruta.media[i].type === "image") {
+                loader.loadMedia(ruta.media[i].contentUrl, function (file) {
+                    var imageUrl = URL.createObjectURL(file);
+                    // const img = document.querySelector('#img');
+                    ReactDOM.render(<img src={imageUrl} alt={"foto" + ruta.fileName} width="500" height="500"/>, img);
+                });
+            }
+
+            if(ruta.media.length>=2){
+                ReactDOM.render(<button className="button" onClick={() => {i--; verMultimedia()}}><img
+                    src="../../../../img/icon/flecha_izquierda.svg"
+                    width="20px"
+                    alt="x"/></button>, buttonL
+                );
+                ReactDOM.render(<button className="button" onClick={() => {i++; verMultimedia()}}>
+                    <img src="../../../../img/icon/flecha_derecha.svg" width="20px"
+                         alt="x"/></button>, buttonR
+                );
+            }
+        }
     }
-
-    
-
-    
 
     function comments() {
         if (ruta.comments.length !== 0) {
