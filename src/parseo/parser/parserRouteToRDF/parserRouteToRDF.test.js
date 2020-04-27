4e3ec476dbc0fcc92993bdb7a6895dae8813d78c
uo237133 <uo237133@uniovi.es>
2020-04-27 20:17:14 +0200

    Solving test

--- src/parseo/parser/parserRouteToRDF/parserRouteToRDF.test.js
+++ src/parseo/parser/parserRouteToRDF/parserRouteToRDF.test.js
@@ -11,7 +11,7 @@ describe.only("Parser from Route to RDF", () => {
         items.push(item);
     }
     const input= new Route("Prueba",items);
-    const output=`@prefix viade: <http://arquisoft.github.io/viadeSpec/>. @prefix: <http://example.org/>. @prefix schema: <http://schema.org/>. @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>. @prefix xsd: <http://www.w3.org/2001/XMLSchema#>. :myRoute a viade:Route ;schema:name "Prueba" ;viade:point [schema:latitude -5.09765625 ;schema:longitude 39.90973623453719 ;viade:order 0] ;viade:point [schema:latitude 3.427734375 ;schema:longitude 46.195042108660154 ;viade:order 1] ;viade:point [schema:latitude 14.414062499999998 ;schema:longitude 51.28940590271679 ;viade:order 2] ;viade:point [schema:latitude 26.54296875 ;schema:longitude 46.49839225859763 ;viade:order 3] .`;
+    const output=`@prefix viade: <http://arquisoft.github.io/viadeSpec/>. @prefix: <http://example.org/>. @prefix schema: <http://schema.org/>. @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>. @prefix xsd: <http://www.w3.org/2001/XMLSchema#>. :myRoute a viade:Route ;schema:name "Prueba";viade:point [schema:latitude -5.09765625 ;schema:longitude 39.90973623453719 ;viade:order 0] ;viade:point [schema:latitude 3.427734375 ;schema:longitude 46.195042108660154 ;viade:order 1] ;viade:point [schema:latitude 14.414062499999998 ;schema:longitude 51.28940590271679 ;viade:order 2] ;viade:point [schema:latitude 26.54296875 ;schema:longitude 46.49839225859763 ;viade:order 3] .`;
 
     test("route to RDF", () => {
         expect(ParserRouteToRDF.parse(input)).toEqual(output);
