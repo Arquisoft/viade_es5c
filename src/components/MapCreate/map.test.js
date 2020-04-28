a25561e5df00027db1fbb833d271c0f37deefb4f
uo237133 <uo237133@uniovi.es>
2020-04-28 22:42:20 +0200

    Test map

--- src/components/MapCreate/map.test.js
+++ src/components/MapCreate/map.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
-import { HashRouter as Router } from 'react-router-dom';
+import {cleanup, render} from 'react-testing-library';
+import {HashRouter as Router} from 'react-router-dom';
 import CreateMap from './Map';
 
 describe.only('CreateMap', () => {
     afterAll(cleanup);
 
-    const { container } = render(
+    const {container} = render(
         <Router>
             <CreateMap/>
         </Router>
@@ -15,4 +15,10 @@ describe.only('CreateMap', () => {
     test('renders without crashing', () => {
         expect(container).toBeTruthy();
     });
+
+    test('clickando en el mapa', () => {
+        container.click();
+        container.click();
+    });
+
 });
