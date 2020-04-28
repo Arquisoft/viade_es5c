569d5ff8eeb9264ae08c49f1ee6f5de07306b566
uo237133 <uo237133@uniovi.es>
2020-04-28 08:08:48 +0200

    [TEST] routesView

--- src/containers/RoutesView/routesView.test.js
+++ src/containers/RoutesView/routesView.test.js
@@ -1,49 +1,69 @@
 import React from 'react';
-import {cleanup} from 'react-testing-library';
-import {configure, render} from 'enzyme';
+import {cleanup, render} from 'react-testing-library';
+import {configure, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {RoutesView} from './RoutesView';
 import {RouteView} from './children/RouteView';
 import Route from "../../entities/Route";
 import Point from "../../entities/Point";
+import {HashRouter as Router} from "react-router-dom";
 
 configure({adapter: new Adapter()});
 
 afterAll(cleanup);
 
-const rutas = [];
+var rutas = [];
+var friends = [];
+var points = [];
 
-function setUp() {
-    const points = [];
-    points.push(new Point(43.354856, -5.851450));
-    points.push(new Point(43.364880, -5.851450));
-    points.push(new Point(43.384900, -5.851450));
+points.push(new Point(43.354856, -5.851450));
+points.push(new Point(43.364880, -5.851450));
+points.push(new Point(43.384900, -5.851450));
 
-    const ruta1 = new Route("ruta1", points);
-    const ruta2 = new Route("ruta2", points);
-    const ruta3 = new Route("ruta3", points);
-    rutas.push(ruta1);
-    rutas.push(ruta2);
-    rutas.push(ruta3);
-}
+rutas.push(new Route("ruta1", points));
+rutas.push(new Route("ruta2", points));
+rutas.push(new Route("ruta3", points));
 
-test('render correctamente', () => {
-    const wrapper = render(<RoutesView rutas={[]}/>);
-    expect(wrapper.find(RoutesView)).toBeDefined();
+const friend1 = {
+    "webId": "friendWebId1",
+    "name": "name1",
+    "image": "image1"
+};
+const friend2 = {
+    "webId": "friendWebId2",
+    "name": "name2",
+    "image": "image2"
+};
+const friend3 = {
+    "webId": "friendWebId3",
+    "name": "name3",
+    "image": "image3"
+};
+friends.push(friend1);
+friends.push(friend2);
+friends.push(friend3);
+
+
+const {container} = render(
+    <Router>
+        <RoutesView rutas={rutas} friends={friends} webId={'viadees5c'}/>
+    </Router>
+);
+
+test('renders without crashing', () => {
+    expect(container).toBeTruthy();
 });
 
-test('render correctamente con rutas vacio', () => {
-    const wrapper = render(<RoutesView rutas={rutas}/>);
+test('render correctamente con arrays vacios', () => {
+    const wrapper = mount(<RoutesView rutas={[]} friends={[]} webId={'viadees5c'}/>);
     expect(wrapper.find(RoutesView)).toBeDefined();
-});
-
-test('render correctamente con rutas lleno', () => {
-    setUp();
-    const wrapper = render(<RoutesView rutas={rutas}/>);
-    expect(wrapper.find(RoutesView)).toBeDefined();
-});
-
-test('Con rutas vacio, num de rutas = 0', () => {
-    const wrapper = render(<RoutesView rutas={rutas}/>);
     expect(wrapper.find(RouteView).length).toBe(0);
 });
+
+test('render correctamente con arrays llenos', () => {
+    const wrapper = mount(<RoutesView rutas={rutas} friends={friends} webId={'viadees5c'}/>);
+    expect(wrapper.find(RoutesView)).toBeDefined();
+    expect(wrapper.find(RouteView).length).not.toBe(0);
+});
+
+
