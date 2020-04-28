8c4c4ca5a0600ece085ca81014270efdcdc5d04b
uo237133 <uo237133@uniovi.es>
2020-04-28 18:43:48 +0200

    [TEST] Ficheros de prueba añadidos

--- test/index.js
+++
@@ -1,5 +0,0 @@
-import geojsoninput from "./parser/geojsoninput.geojson";
-import gpxinput from "./parser/gpxinput.gpx";
-import kmlinput  from "./parser/kmlinput.kml"
-
-export {geojsoninput, gpxinput, kmlinput};
