7ab5fe1e475b0df6f01ad08f6d0c241d5077c4b5
uo237133 <uo237133@uniovi.es>
2020-04-28 14:10:18 +0200

    [TEST] parserJSONld

--- src/parseo/parser/parserToRoute/parserJsonLd.test.js
+++ src/parseo/parser/parserToRoute/parserJsonLd.test.js
@@ -1,7 +1,20 @@
 import Route from "../../../entities/Route";
-import Point from "../../../entities/Point";
-import  JsonldToRouteParser from "./parserJsonLd";
+import JsonldToRouteParser from "./parserJsonLd";
+import jsonld from '../../../../test/parser/jsonld'
 
-test("true", () => {
-    expect(true).toBeTruthy();
+test('Parser JSONld to Route', () => {
+    let parserclass = new JsonldToRouteParser('miwebid', JSON.stringify(jsonld));
+    const output = new Route("Route test 1", [{"position": {"lat": 45.123, "lng": 34.121}}, {
+        "position": {
+            "lat": 46.123,
+            "lng": 34.121
+        }
+    }, {"position": {"lat": 47.123, "lng": 34}}, {
+        "position": {
+            "lat": 48.123,
+            "lng": 32.121
+        }
+    }], 'Descripcion', [{"comment": {"createdAt": 34.121, "text": "comentario"}}]);
+    output.setImg('http://inrupt.luispc1998/viade/resources/da34fas749sa3h883j.jpg');
+    expect(parserclass.parse()).toEqual(output);
 });
